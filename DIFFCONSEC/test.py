e5d1a4c7c706aa915294670473375a2add2befec
Ahmed <105589873+ahmedkerouat@users.noreply.github.com>
2022-10-25 06:30:56 +0100

    Update test.py

--- DIFFCONSEC/test.py
+++ DIFFCONSEC/test.py
@@ -3,15 +3,9 @@ if 1 <= T <= 100:
     for i in range(1, T + 1):
         N = int(input())
         S = input()
-        output = 0
-        count = 0
-        for x in range(len(S)):
-            if int(S[x]) == 0:
-                count += 1
-                if count == 2:
-                    output += 1
-            else:
-                count -= 1
-                if count == -2:
-                    output += 1
-        print(output)
+        c = 0
+        for x in range(N - 1):
+            print(x)
+            if S[x] == S[x+1]:
+                c += 1
+        print(c)
