b5a12f5b21fa7ed7f2398c1d60a165e7ccde31a9
Ahmed <105589873+ahmedkerouat@users.noreply.github.com>
2022-12-03 21:15:42 +0100

    solved some problems

---
+++ template/template.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int N;
+    cin >> N; // Number of test cases
+    for (int i = 0; i < N; i++) {
+        // Do stuff
+    }
+    return 0;
+}
