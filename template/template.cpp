796e606604513be6ada4527efa7a9317871eae27
Ahmed <105589873+ahmedkerouat@users.noreply.github.com>
2023-02-04 19:10:22 +0100

    Update template.cpp

--- template/template.cpp
+++ template/template.cpp
@@ -1,12 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+
 #define mp make_pair
-#define ll long long
 #define pb push_back
 #define fi first
 #define se second
 #define endl "\n"
+#define INF 1000000000
+#define MOD 1000000007
 
 int solve() {
     return 0;
@@ -14,7 +20,9 @@ int solve() {
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("input", "r", stdin);
+    //freopen("output", "w", stdout);
     int t;
     cin >> t;
     while (t--) {
