4172d178fed89ce9b1811c61730142a975f8c987
Ahmed <cmoiminp@gmail.com>
2023-03-15 11:40:35 +0000

    solved a problem

---
+++ practice/UVA-11450(top-down).cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define endl "\n"
+#define INF 1000000000
+#define MOD 1000000007
+
+int solve() {
+    return 0;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    //freopen("input", "r", stdin);
+    //freopen("output", "w", stdout);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
